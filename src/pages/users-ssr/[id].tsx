77d36c3ee1cff6ca905b234ea7c45e400b17c6a6
Mick Coelho <mick.coelho@b-reel.com>
2021-03-26 12:07:08 -0700

    Remove getStaticPaths

--- src/pages/users-ssr/[id].tsx
+++ src/pages/users-ssr/[id].tsx
@@ -34,18 +34,6 @@ const StaticPropsDetail: FunctionComponent<null> = ({
 
 export default StaticPropsDetail;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  // Get the paths we want to pre-render based on users
-  const users = await getUsers();
-  const paths = users.map((user: User) => ({
-    params: { id: user.id },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-};
-
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries.
