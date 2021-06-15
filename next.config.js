734da53075cc81d365ce980b60de02b0b3e11eb1
Mick Coelho <mick.coelho@b-reel.com>
2021-06-15 11:23:43 -0700

    Update classNames

--- next.config.js
+++ next.config.js
@@ -6,28 +6,4 @@ module.exports = {
     locales: ['en', 'sv'],
     defaultLocale: 'en',
   },
-  webpack: (config, { dev }) => {
-    config.resolve.alias.react = path.join(__dirname, 'node_modules/react');
-
-    // https://github.com/vercel/next.js/issues/10584#issuecomment-766183646
-    config.module.rules[1].oneOf.forEach((moduleLoader, i) => {
-      // eslint-disable-next-line no-unused-expressions
-      Array.isArray(moduleLoader.use) &&
-        moduleLoader.use.forEach((l) => {
-          if (
-            l.loader.includes('css-loader') &&
-            !l.loader.includes('postcss-loader')
-          ) {
-            l.options = {
-              ...l.options,
-              modules: {
-                ...l.options.modules,
-                exportLocalsConvention: 'camelCaseOnly',
-              },
-            };
-          }
-        });
-    });
-    return config;
-  },
 };
